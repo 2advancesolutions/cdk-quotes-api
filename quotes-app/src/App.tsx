da8e3092685e64ecae6eb6a6f0ebb24f8538eb5c
batman <2advancesolutions@gmail.com>
2023-01-16 21:58:20 -0600

    add chakra lib react project

--- quotes-app/src/App.tsx
+++ quotes-app/src/App.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Quotes from './componets/Quotes';
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
+// `@chakra-ui/theme` is a part of the base install with `@chakra-ui/react`
+import chakraTheme from '@chakra-ui/theme'
+
+const { Button } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
+
 
 function App() {
   return (
+    <ChakraProvider theme={theme}>
     <div className="App">
       <Quotes />
     </div>
+    </ChakraProvider>
   );
 }
 
--- quotes-app/src/componets/Quotes.tsx
+++ quotes-app/src/componets/Quotes.tsx
@@ -1,37 +1,66 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 interface Quote {
-    id: number;
-    text: string;
-    author: string;
+  id: number;
+  quote: string;
+  author: string;
 }
 
-interface Props {}
+const Quotes: React.FC = () => {
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [loading, setLoading] = useState(true);
 
-const Quotes: React.FC<Props> = () => {
-    const [quotes, setQuotes] = useState<Quote[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://e1qo3ccpmb.execute-api.us-east-2.amazonaws.com/prod/quotes');
+        const data = await res.json();
+        setQuotes(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        axios.get<Quote[]>('https://e1qo3ccpmb.execute-api.us-east-2.amazonaws.com/prod/quotes')
-            .then(response => {
-                setQuotes(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, []);
-
-    return (
-        <div>
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <Table>
+          <Thead>
+            <Tr>
+              <Td>ID</Td>
+              <Td>Quote</Td>
+              <Td>Author</Td>
+            </Tr>
+          </Thead>
+          <Tbody>
             {quotes.map((quote: Quote) => (
-                <div key={quote.id}>
-                    <p>{quote.text}</p>
-                    <p>{quote.author}</p>
-                </div>
+              <Tr key={quote.id}>
+                <Td>{quote.id}</Td>
+                <Td>{quote.quote}</Td>
+                <Td>{quote.author}</Td>
+              </Tr>
             ))}
-        </div>
-    );
-}
+          </Tbody>
+        </Table>
+      )}
+    </div>
+  );
+};
 
 export default Quotes;
